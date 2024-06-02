88d3b0bb987f615fa64446c98d2890e97f5133a1
Maxi Peszano <imaxi93@gmail.com>
2024-06-01 23:27:29 -0300

    fix cart state var type

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,13 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   // cart: [],
-  cart: {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
 };
 
 const cartSlice = createSlice({
